ed0691f67cdd84cce0fc8aa53c5be0c3c116390f
RodrigoRaulQuicaño <165697892+MrRod2819@users.noreply.github.com>
2025-09-13 12:24:58 -0500

    Add files via upload

---
+++ Examen/Segunda Pregunta/script.js
@@ -0,0 +1,11 @@
+
+
+
+function calcular(){
+    const x = document.getElementById("Temperatura").value
+    const resultado = (x-32)*5/9
+    alert("La temperatura es de: " + resultado)
+    
+}
+
+
